fcdcaa11cac2340e0f91afc54a091bb19e97fcc0
justincong <justincong324@gmail.com>
2019-11-10 13:54:46 -0500

    mic

--- mic/mic.ino
+++ mic/mic.ino
@@ -4,11 +4,41 @@ AKW Installation
 Mic
 
 */
-
-void setup() {
-
+int led = 13;
+int micPin = 33;
+int switchPin = 21;
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(switchPin, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
 }
 
-void loop(){
-  
+int switchState;
+ 
+void loop()
+{
+    switchState = digitalRead(switchPin);
+    
+    if (switchState){
+
+      digitalWrite(led, HIGH);
+      long sum = 0;
+      
+      for(int i=0; i<1000; i++)
+      {
+          sum += analogRead(micPin);
+      }
+ 
+      sum = sum/1000;
+   
+      Serial.println(sum);
+   
+      delay(10);
+    }
+    else {
+      digitalWrite(led, LOW);
+    }
+   
 }
