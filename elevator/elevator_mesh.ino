4da571b0782323c959b27e5f124cb96fde02f5aa
vgr-nightfairy <55374515+vgr-nightfairy@users.noreply.github.com>
2019-11-10 15:16:28 -0500

    Mesh network sending elevator ultrasonic data

---
+++ elevator/elevator_mesh.ino
@@ -0,0 +1,106 @@
+//************************************************************
+// this is a simple example that uses the painlessMesh library
+//
+// 1. sends a silly message to every node on the mesh at a random time between 1 and 5 seconds
+// 2. prints anything it receives to Serial.print
+//
+//
+//************************************************************
+#include <painlessMesh.h>
+
+#define   MESH_PREFIX     "Atrium AKW"
+#define   MESH_PASSWORD   "lets go!"
+#define   MESH_PORT       5555
+
+// Prototypes
+void sendMessage(); 
+void receivedCallback(uint32_t from, String & msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback(); 
+void nodeTimeAdjustedCallback(int32_t offset); 
+void delayReceivedCallback(uint32_t from, int32_t delay);
+
+Scheduler     userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+const int trigPin = 12;
+const int echoPin = 13;
+ 
+long duration;
+long distance;
+
+// User stub
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+
+void sendMessage() {
+  String msg = "Elevator";
+  msg += mesh.getNodeId();
+  msg += String(distance);
+  mesh.sendBroadcast( msg );
+  taskSendMessage.setInterval( TASK_SECOND * 1 );
+}
+
+// Needed for painless library
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+}
+
+void loop() {
+  // it will run the user scheduler as well
+  mesh.update();
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  //Reads the echoPin and the time it
+  //took for the HIGH pulse to return
+  duration = pulseIn(echoPin, HIGH);
+ 
+  //Calculating the distance in in
+  distance = (duration/2) / 74;
+ 
+  //Checking to see if the discance is less than 400
+  //to avoid getting alerts when object is out of range.
+  if (distance < 400){
+    Serial.print("distance: ");
+    Serial.println(distance);
+  }
+ 
+  delay(200);
+}
